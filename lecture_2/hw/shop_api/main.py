4444672e9e74557c2fd6bf49408206425fc37601
Pavel Mokeev <p.mokeev@skbkontur.ru>
2024-10-06 12:00:15 +0300

    [patch] Добавлен prometheus и grafana

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from lecture_2.hw.shop_api.config import API_PREFIX, PROJECT_NAME
 from lecture_2.hw.shop_api.routers import carts_router, items_router
@@ -7,5 +8,6 @@ from lecture_2.hw.shop_api.storage import init_db
 app = FastAPI(title=PROJECT_NAME)
 app.include_router(prefix=API_PREFIX, router=items_router, tags=["Item"])
 app.include_router(prefix=API_PREFIX, router=carts_router, tags=["Cart"])
+Instrumentator().instrument(app).expose(app)
 
 init_db()
