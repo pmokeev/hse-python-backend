003bee66766ba5468013b85a88e763fb2ed41fd0
Pavel Mokeev <p.mokeev@skbkontur.ru>
2024-10-10 22:48:32 +0300

    Минорные фиксы

--- ddos.js
+++ ddos.js
@@ -10,9 +10,9 @@ export const options = {
       timeUnit: '1s',
       startRate: 50,
       stages: [
-        { target: 200, duration: '5m' }, // linearly go from 50 iters/s to 200 iters/s for 3m
+        { target: 200, duration: '5m' }, // linearly go from 50 iters/s to 200 iters/s for 5m
         { target: 500, duration: '0' }, // instantly jump to 500 iters/s
-        { target: 500, duration: '5m' }, // continue with 500 iters/s for 3 minutes
+        { target: 500, duration: '5m' }, // continue with 500 iters/s for 5 minutes
       ],
     },
   },
@@ -22,8 +22,8 @@ export default function () {
   const uid = `${exec.scenario.iterationInTest}`
 
   let body = {
-    username: String(uid),
-    name: String(uid),
+    username: uid,
+    name: uid,
     birthdate: "2024-10-09T00:00:00",
     password: "password123"
   };
@@ -32,6 +32,6 @@ export default function () {
 
   check(res, {
     'status is 200': (r) => r.status === 200,
-    'is correct username': (r) => r.json().username === String(uid),
+    'is correct username': (r) => r.json().username === uid,
   });
 }
